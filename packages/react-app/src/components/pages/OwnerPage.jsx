93b30814c89fe181c460b05dcc7f77b64ba5f163
andrewhong5297 <47720952+andrewhong5297@users.noreply.github.com>
2020-10-19 14:50:22 -0400

    trying to fix netlify

--- packages/react-app/src/components/pages/OwnerPage.jsx
+++ packages/react-app/src/components/pages/OwnerPage.jsx
@@ -5,24 +5,24 @@ export const OwnerPage = (props) => {
     let projectName,budgets,timeline;
     const [textArea, setText] = useState("loading...")
 
-    const getProjectDetails= async () => {
-        projectName = await props.firstEscrow.projectName()
-        budgets = await props.firstEscrow.budgetsOne()
-        timeline = await props.firstEscrow.timelineOne()
+    // const getProjectDetails= async () => {
+    //     projectName = await props.firstEscrow.projectName()
+    //     budgets = await props.firstEscrow.budgetsOne()
+    //     timeline = await props.firstEscrow.timelineOne()
 
-        setText(
-            <div>
-                <div>Project Name: {projectName}</div>
-                <div>Milestone One Budget: {budgets.toString()}</div>
-                <div>Timeline for Milestone One (months): {timeline.toString()}</div>
-            </div>
-        )
+    //     setText(
+    //         <div>
+    //             <div>Project Name: {projectName}</div>
+    //             <div>Milestone One Budget: {budgets.toString()}</div>
+    //             <div>Timeline for Milestone One (months): {timeline.toString()}</div>
+    //         </div>
+    //     )
             
-    }
+    // }
 
-    useEffect(() => {
-        getProjectDetails();
-      }, []);
+    // useEffect(() => {
+    //     getProjectDetails();
+    //   }, []);
     
         return ( 
             <React.Fragment>
